741df4938f0cad5fe72eb89ef9b1f15a1d371437
ziching <ZichengZh@live.com>
2018-06-21 19:50:34 +0800

    添加初学者项目示例

---
+++ SignalR-Beginners/SignalRChat.Beginner1.0/MyChatHub.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRChat.ChatHub
+{
+    public class MyChatHub:Hub
+    {
+        /// <summary>
+        /// 发送消息
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public async Task SendMessage(string userName,string message)
+        {
+            await Clients.All.SendAsync("SendMessage",userName, message);
+        }
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
